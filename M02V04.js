efed107e7f3bd87c52a877b86e5f02a24caebfe5
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-11-04 00:23:46 +0600

    from fill

--- M02V04.js
+++ M02V04.js
@@ -33,6 +33,31 @@ const hasEvenNumber = numbers.some((num) => num % 2 == 0);
 
 // const arr = Array.from({ length: 5 }).fill(0);
 // console.log(arr); // [ 0, 0, 0, 0, 0 ]
+// or
+// const arr = Array.from({ length: 5 }).fill("ABC");
+// console.log(arr); // [ "ABC", "ABC", "ABC", "ABC", "ABC" ]
+// or
+// Create an array of specific length and fill it with a value
+// const arr = Array.from({ length: 5 }, (_, index) => index);
+// console.log(arr); // [ 0, 1, 2, 3, 4 ]
+// or
+// const arr = Array.from([1, 2, 3, 4], (value, index) => value * value);
+// console.log(arr); //[ 1, 4, 9, 16 ]
 
-const arr = Array.from({ length: 5 }).fill("ABC");
-console.log(arr); // [ "ABC", "ABC", "ABC", "ABC", "ABC" ]
+// Create an array of specific length and fill it with a value
+// const range = (start, stop, step) =>
+//   Array.from(
+//     { length: Math.ceil((stop - start) / step) },
+//     (_, i) => start + i * step
+//   );
+// console.log(range(1, 11, 2));
+//[ 1, 3, 5, 7, 9 ]
+
+//or
+const range = (start, stop, step) =>
+  Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * step
+  );
+console.log(range(0, 11, 2));
+//[ 0, 2, 4, 6, 8, 10 ]
